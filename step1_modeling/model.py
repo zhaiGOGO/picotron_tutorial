9839c83f33001f18c7a16872850a4843f625d1b6
ferdinand.mom <ferdinand.mom@epita.fr>
2024-11-16 00:54:57 +0000

    cleaner modeling

--- step1_modeling/model.py
+++ step1_modeling/model.py
@@ -1,4 +1,3 @@
-import os
 import torch 
 import torch.nn as nn
 import torch.nn.functional as F
@@ -6,12 +5,18 @@ from flash_attn.flash_attn_interface import flash_attn_func
 from flash_attn.layers.rotary import apply_rotary_emb
 from flash_attn.ops.triton.layer_norm import layer_norm_fn
 
+def flash_attention(q, k, v, causal = True):
+    q = q.permute(0, 2, 1, 3) # [batch_size, seq_length, num_head , head_dim]
+    k = k.permute(0, 2, 1, 3) # [batch_size, seq_length, num_head , head_dim]
+    v = v.permute(0, 2, 1, 3) # [batch_size, seq_length, num_head , head_dim]
+    return flash_attn_func(q, k, v, causal=causal)
+
 def get_cos_sin(seq_length, head_dim, base=500000.0):
     assert head_dim%2==0
     # Results on CUDA and CPU are different even with the same formula, To match transformers implementation. frequency should be computed on CPU
     theta = 1.0 / (base ** (torch.arange(0, head_dim, 2, dtype=torch.int64).float().to('cpu') / head_dim))
-    dtype = torch.bfloat16 if os.getenv('DTYPE', 'bfloat16') == 'bfloat16' else torch.float32
-    device = torch.device('cuda') if os.getenv('DEVICE', 'cuda') == 'cuda' else torch.device('cpu')
+    dtype = torch.bfloat16
+    device = torch.device('cuda')
     position = torch.arange(seq_length).to(device).unsqueeze(1).float() # [seq_length, 1]
     # To match transformers implementation. m * theta should be computed on GPU
     theta = theta.to(device)
@@ -19,7 +24,6 @@ def get_cos_sin(seq_length, head_dim, base=500000.0):
 
 class TritonRMSNorm(nn.Module):
     def __init__(self, hidden_size, eps=1e-5, device=None, dtype=None):
-        factory_kwargs = {"device": device, "dtype": dtype}
         super().__init__()
         self.eps = eps
         self.weight = nn.Parameter(torch.ones(hidden_size))
@@ -57,8 +61,6 @@ class Attention(nn.Module):
         self.out_proj = nn.Linear(config.hidden_size, config.hidden_size, bias=False)
         self.layer_idx = layer_idx
         
-        ## TODO support mask
-    
     def forward(self, x, cos, sin, attention_mask=None, position_ids=None):
         batch_size, seq_length, hidden_dim = x.size()
         q = self.q_proj(x) # [batch_size, seq_length, num_heads*head_dim]
@@ -77,15 +79,12 @@ class Attention(nn.Module):
         v = v.repeat_interleave(self.num_local_heads // self.num_local_kv_heads, dim=1) # [batch_size, num_heads, seq_length, head_dim]
         
         causal = True if q.size(2) == k.size(2) else False # During decoding phase. The lenghth of q is usually 1.    
-    
-        # Pytorch scaled dot product attention
-        out = F.scaled_dot_product_attention(q, k, v, is_causal=causal) # [batch_size, num_heads, seq_length, head_dim]
-        out = out.transpose(1, 2) # [batch_size, seq_length, num_heads, head_dim]
-    
+
+        out = flash_attention(q, k, v, causal = causal) # [batch_size, seq_length, num_heads, head_dim] 
+
         out = out.reshape(batch_size, seq_length, self.num_local_heads * self.head_dim) # [batch_size, seq_length, hidden_dim]
         out = self.out_proj(out) # [batch_size, seq_length, hidden_dim]
         return out
-
 
 class MLP(nn.Module):
     def __init__(self, config) -> None:
@@ -111,7 +110,6 @@ class DecoderLayer(nn.Module):
         self.cos, self.sin = get_cos_sin(config.max_position_embeddings, head_dim=head_dim , base=config.rope_theta) # [max_position_embeddings, head_dim]
 
     def forward(self, x, attention_mask = None, position_ids = None):
-        #TODO: Use the default position_ids for RoPE during training. If we have time, work on generation
         cos, sin = self.cos, self.sin 
         x = x + self.attention(self.input_layernorm(x), cos, sin, attention_mask, position_ids) # Attention 
         x = x + self.mlp(self.post_attention_layernorm(x)) # MLP
