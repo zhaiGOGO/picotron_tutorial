ca8618d99e5d72d88ec541ab7d496d28d38a42a4
ferdinand.mom <ferdinand.mom@epita.fr>
2024-11-17 15:47:43 +0000

    add patch step 2

--- step1_modeling/train.py
+++ step1_modeling/train.py
@@ -22,17 +22,18 @@ if __name__ == "__main__":
 
     # Model arguments
     parser.add_argument("--model_name", type=str, default="HuggingFaceTB/SmolLM-360M-Instruct")
-    parser.add_argument("--num_hidden_layers", type=int, default=16)
+    parser.add_argument("--num_hidden_layers", type=int, default=32)
     parser.add_argument("--num_attention_heads", type=int, default=16)
     parser.add_argument("--num_key_value_heads", type=int, default=4)
-    
+
     # Training arguments
     parser.add_argument("--seed", type=int, default=42)
     parser.add_argument("--learning_rate", type=float, default=3e-4)
-    parser.add_argument("--seq_length", type=int, default=2048)
+    parser.add_argument("--seq_len", type=int, default=32)
     parser.add_argument("--micro_batch_size", type=int, default=1)
 
     # Logging arguments
+    parser.add_argument("--run_name", type=str, default="default_run")
     parser.add_argument("--use_wandb", action="store_true")
 
     args = parser.parse_args()
@@ -58,7 +59,7 @@ if __name__ == "__main__":
     model_config.num_hidden_layers = args.num_hidden_layers
     model_config.num_attention_heads = args.num_attention_heads
     model_config.num_key_value_heads = args.num_key_value_heads
-    model_config.max_position_embeddings = args.seq_length
+    model_config.max_position_embeddings = args.seq_len
 
     model = Llama(config=model_config)
     model.to(dtype).to(device)            
@@ -71,8 +72,8 @@ if __name__ == "__main__":
     dist.barrier()
     
     # Create dummy data
-    input_ids = torch.randint(0, model_config.vocab_size, (args.micro_batch_size, args.seq_length), device=device)
-    target_ids = torch.randint(0, model_config.vocab_size, (args.micro_batch_size, args.seq_length), device=device)
+    input_ids = torch.randint(0, model_config.vocab_size, (args.micro_batch_size, args.seq_len), device=device)
+    target_ids = torch.randint(0, model_config.vocab_size, (args.micro_batch_size, args.seq_len), device=device)
 
     # Training step
     optimizer.zero_grad()
